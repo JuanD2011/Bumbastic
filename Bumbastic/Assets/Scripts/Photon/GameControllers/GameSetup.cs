ceed185833b05e571b7eefaf8ddf8a6c3da609c5
JuanD2011 <juan.atehortual@upb.edu.co>
2019-02-01 11:50:04 -0500

    Photon avatar/2

---
+++ Bumbastic/Assets/Scripts/Photon/GameControllers/GameSetup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour
+{
+    public static GameSetup gameSetup;
+
+    public Transform[] spawnPoints;
+
+    private void OnEnable() 
+    {
+        if(GameSetup.gameSetup == null) 
+        {
+            GameSetup.gameSetup = this;
+        }       
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Bumbastic/Assets/Scripts/Photon/GameControllers/PhotonPlayer.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using System.IO;
+
+public class PhotonPlayer : MonoBehaviour
+{
+    private PhotonView pV;
+    public GameObject myAvatar;
+
+    void Start()
+    {
+        pV = GetComponent<PhotonView>();
+        int spawnPicker = Random.Range(0, GameSetup.gameSetup.spawnPoints.Length);
+
+        if(pV.IsMine) 
+        {
+            myAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Bummie Variant"), GameSetup.gameSetup.spawnPoints[spawnPicker].position, GameSetup.gameSetup.spawnPoints[spawnPicker].rotation, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
