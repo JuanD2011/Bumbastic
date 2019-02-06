13e8c2290918bad0cbb84996652b0b1fcee27e75
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-02-05 20:03:23 -0500

    Crow

---
+++ Bumbastic/Assets/Scripts/Animation/Crow/Crow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crow : StateMachineBehaviour
+{
+    float t = 0f;
+    float tDrop = 0f;
+    [SerializeField] float flyingVel;
+    [SerializeField] float tSpawnPU = 5f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
+    {
+        tDrop = 0f;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        t += Time.deltaTime;
+        tDrop += Time.deltaTime;
+        animator.transform.parent.transform.eulerAngles = new Vector3(0, t * flyingVel, 0);
+        if (tDrop > tSpawnPU) {
+            animator.SetTrigger("DropPU");
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        GameManager.instance.CrowPos = animator.transform.localPosition;
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Bumbastic/Assets/Scripts/Animation/Crow/FlyUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyUp : StateMachineBehaviour
+{
+    //Vector3 crowPos;
+    //public Vector3 CrowPos { get => crowPos; set => crowPos = value; }
+    [SerializeField] float timeToUp;
+    float distance;
+    Vector3 target;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
+    {
+        target = new Vector3(-GameManager.instance.CrowPos.x, GameManager.instance.CrowPos.y, 0f);
+        distance = Vector3.Distance(animator.transform.position, target);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.localPosition = Vector3.MoveTowards(animator.transform.localPosition, target, (Time.deltaTime * distance) / timeToUp);
+        if (animator.transform.localPosition == target)
+        {
+            animator.SetTrigger("IsFlying");
+        }
+    }
+}
