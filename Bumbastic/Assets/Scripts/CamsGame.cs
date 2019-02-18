903feb4d4735141a808866e31259323ede709171
danialarcon0698 <daniel.alarcon@upb.edu.co>
2019-02-18 09:42:01 -0500

    WIP CamFlow

---
+++ Bumbastic/Assets/Scripts/CamsGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CamsGame : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera[] virtualCameras;
+
+    private void Start()
+    {
+        for (int i = 0; i < GameManager.instance.PlayersInGame.Count; i++)
+        {
+            if (GameManager.instance.PlayersInGame[i].transform.GetComponentInChildren<Bomb>() == null)
+            {
+                virtualCameras[i].LookAt = GameManager.instance.PlayersInGame[i].transform; 
+            }
+            else
+                virtualCameras[i] = virtualCameras[virtualCameras.Length - 1];
+        }
+    }
+}
